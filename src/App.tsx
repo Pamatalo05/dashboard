405089b9f0f923ac4c169c4ebb9ca5afcf1b331a
Paulo Tapia <pmtapia@espol.edu.ec>
2025-11-12 18:59:09 +0000

    GUIA PARTE III

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import { Grid } from '@mui/material';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [] = useState(0)
 
   return (
     <div >
